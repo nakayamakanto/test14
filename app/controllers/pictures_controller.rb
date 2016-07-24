0b18fbc83fb76fefb7e871d61ace1807e8ba022b
snakayama <hancldrsn@gmail.com>
2016-07-24 15:52:53 +0900

    picture done

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -71,6 +71,6 @@ class PicturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def picture_params
-      params.require(:picture).permit(:title, :url, :content)
+      params.require(:picture).permit(:title, :image, :content)
     end
 end
